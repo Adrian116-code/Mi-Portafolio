f87f4c7edf5495c85a317e20e220f0d2b66165ba
Adrian <delapuenteadrian3@gmail.com>
2025-12-28 18:42:50 -0500

    actualización python

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HostListener } from '@angular/core';
 declare var AOS: any;
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -15,6 +17,33 @@ export class App {
     
     AOS.init({
     });
+   this.isPc = window.innerWidth > 768;
   }
+
+  MostrarAngular: boolean = false;
+
+  MostrarSpring: boolean = false;
+
+  MostrarPython: boolean = false;
+
+  toggleAngular(){
+    this.MostrarAngular = !this.MostrarAngular;
+  }
+
+  toggleSpring(){
+    this.MostrarSpring = !this.MostrarSpring;
+  }
+
+  togglePython(){
+    this.MostrarPython = !this.MostrarPython;
+  }
+
+  isPc: boolean = false;
+
+ @HostListener('window:resize')
+  onResize() {
+    this.isPc = window.innerWidth > 1024;
+  }
+  
   
 }
