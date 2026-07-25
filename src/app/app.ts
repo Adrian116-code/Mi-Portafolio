3f6dd0ba5aa67ea24101187e185452ef5935e55a
Adrian <delapuenteadrian3@gmail.com>
2026-07-25 12:59:49 -0500

    Actualización Sliders

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,23 +1,26 @@
-import { Component, signal, OnInit } from '@angular/core';
+import { Component, signal, OnInit, ViewChild, ElementRef, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HostListener } from '@angular/core';
+import { register } from 'swiper/element/bundle';
+register();
 declare var AOS: any;
 
 @Component({
   selector: 'app-root',
   imports: [CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 
 export class App {
   protected readonly title = signal('portafolio');
-  
+
   ngOnInit(): void {
-    
+
     AOS.init({
     });
-   this.isPc = window.innerWidth > 768;
+    this.isPc = window.innerWidth > 768;
   }
 
   MostrarAngular: boolean = false;
@@ -25,6 +28,10 @@ export class App {
   MostrarSupabase: boolean = false;
 
   MostrarPython: boolean = false;
+
+  MostrarUnity: Boolean = false;
+
+  MostrarAndroid: Boolean = false;
 
   toggleAngular(){
     this.MostrarAngular = !this.MostrarAngular;
@@ -38,9 +45,17 @@ export class App {
     this.MostrarPython = !this.MostrarPython;
   }
 
+  toggleUnity(){
+    this.MostrarUnity = !this.MostrarUnity;
+  }
+
+  toggleAndroid(){
+    this.MostrarAndroid = !this.MostrarAndroid;
+  }
+
   isPc: boolean = false;
 
- @HostListener('window:resize')
+  @HostListener('window:resize')
   onResize() {
     this.isPc = window.innerWidth > 1024;
   }
